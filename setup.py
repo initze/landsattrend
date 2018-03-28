af9ff9e2cdcd89beeec301ac5405d6464d143284
Ingmar Nitze <ingmar.nitze@awi.de>
2018-03-28 14:56:40 +0200

    minor addition to setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,20 @@ setup(
     version='0.1.0',
     description='Package to preprocess and create trends on Landsat time-series',
     author='Ingmar Nitze',
-    author_email='ingmar.nitze@awi.de'
-    #install_requires=['numpy>=1.13', 'pandas>=0.20', 'geopandas>=0.3', 'netCDF4>=1.3', 'gdal>=2.0', 'rasterio>=0.36', 'fiona>=1.7', 'joblib>=0.11', 'bottleneck>=1.2', 'scipy>=0.19']
+    author_email='ingmar.nitze@awi.de',
+    package_dir={'': '.'},
+    packages=['landsattrend'])
+    #install_requires=['numpy>=1.13',
+    # 'pandas>=0.20', 'geopandas>=0.3',
+    # 'netCDF4>=1.3', 'gdal>=2.0',
+    # 'rasterio>=0.36',
+    # 'fiona>=1.7',
+    # 'joblib>=0.11',
+    # 'bottleneck>=1.2',
+    # 'scipy>=0.19',
+    # 'pyproj',
+    # 'shapely',
+    # 'sklearn',
+    # 'skimage']
     # non-pypi packages
     # dependency_links=[]
-)
