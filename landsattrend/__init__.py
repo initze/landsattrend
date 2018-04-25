e6f40c8b8388271fba19fd39bc6251a8ae93ab40
Ingmar Nitze <ingmar.nitze@awi.de>
2018-04-25 15:06:23 +0200

    import fix

--- landsattrend/__init__.py
+++ landsattrend/__init__.py
@@ -5,10 +5,10 @@ Description
 """
 
 
-from .processing_funcs import LocPreProcessor, LocPreProcessorDEM, \
+from landsattrend.processing_funcs import LocPreProcessor, LocPreProcessorDEM, \
     Processor, auto_prlist
-from .data_stack import DataStack
-from .mosaic import Mosaic, MosaicFiltered, MosaicNewOnly
-from .config_study_sites import study_sites
-from .file_preprocessor import FilePreProcessor
+from landsattrend.data_stack import DataStack
+from landsattrend.mosaic import Mosaic, MosaicFiltered, MosaicNewOnly
+from landsattrend.config_study_sites import study_sites
+from landsattrend.file_preprocessor import FilePreProcessor
 from .version import __version__
