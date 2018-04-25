9068c1ded39b47dc24896902d701d683d25d6d03
Ingmar Nitze <ingmar.nitze@awi.de>
2018-04-25 15:08:35 +0200

    reverted init

--- landsattrend/__init__.py
+++ landsattrend/__init__.py
@@ -6,8 +6,7 @@ Description
 
 
 from landsattrend.processing_funcs import LocPreProcessor, LocPreProcessorDEM, \
-    Processor, auto_prlist
-from landsattrend.data_stack import DataStack
+    Processor, DataStack, auto_prlist
 from landsattrend.mosaic import Mosaic, MosaicFiltered, MosaicNewOnly
 from landsattrend.config_study_sites import study_sites
 from landsattrend.file_preprocessor import FilePreProcessor
