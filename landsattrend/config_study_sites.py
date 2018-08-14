438452e65215e63288913cc93b13730225bbaf4b
Ingmar Nitze <ingmar.nitze@awi.de>
2018-08-14 10:58:11 +0200

    config loader

--- landsattrend/config_study_sites.py
+++ landsattrend/config_study_sites.py
@@ -1,5 +1,6 @@
 __author__ = 'initze'
 import os
+import pandas as pd
 
 
 DATA_DIR = r'N:\petacarbnob1\petacarb\initze\01_Data_Archive\02_Processed_Masked'
@@ -9,6 +10,11 @@ PROCESSING_DIR_02 = r'K:\01_RasterData\01_Landsat\02_Processed'
 VECTOR_DIR = r'D:\05_Vector'
 RESULT_DIR = r'E:\06_Trendimages'
 
+# load study sites from configuration file
+csvdir = os.path.join(os.environ['HOMEDRIVE'], os.environ['HOMEPATH'], 'landsattrend')
+csvpath = os.path.join(csvdir, 'config_study_sites.csv')
+study_sites = pd.DataFrame.from_csv(csvpath).T.to_dict()
+"""
 study_sites = {
 
     'GFZ-Brasil-1':{
@@ -453,4 +459,6 @@ study_sites = {
         'epsg': 32653,
         'bbox': None},
 }
+
+"""
 wrs2_path = os.path.join(VECTOR_DIR, 'wrs2_descending.shp')
