2d6292d44acf69d974314830e807a0f33a8358bb
initze <ingmarnitzegmail.com>
2020-09-02 14:16:11 +0200

    initial commit of basic working version

--- landsattrend/config_study_sites.py
+++ landsattrend/config_study_sites.py
@@ -2,16 +2,16 @@ __author__ = 'initze'
 import os
 import pandas as pd
 
-
-DATA_DIR = r'N:\petacarbnob1\petacarb\initze\01_Data_Archive\02_Processed_Masked'
-DEM_DIR = r'D:\01_RasterData\01_Landsat\04_DEM'
-PROCESSING_DIR_01 = r'D:\01_RasterData\01_Landsat\02_Processed'
-PROCESSING_DIR_02 = r'K:\01_RasterData\01_Landsat\02_Processed'
-VECTOR_DIR = r'D:\05_Vector'
-RESULT_DIR = r'E:\06_Trendimages'
+BASE_DIR = os.getcwd()
+DATA_DIR = os.path.join(BASE_DIR, 'dataX')
+DEM_DIR = os.path.join(BASE_DIR, 'aux_data', 'dem')
+PROCESSING_DIR_01 = os.path.join(BASE_DIR, 'processing')
+PROCESSING_DIR_02 = os.path.join(BASE_DIR, 'processing')
+VECTOR_DIR = os.path.join(BASE_DIR, 'vector')
+RESULT_DIR = os.path.join(BASE_DIR, 'data')
 
 # load study sites from configuration file
-csvdir = os.path.join(os.environ['HOMEDRIVE'], os.environ['HOMEPATH'], 'landsattrend')
+csvdir = os.path.join(os.getcwd(),'config')
 csvpath = os.path.join(csvdir, 'config_study_sites.csv')
 study_sites = pd.DataFrame.from_csv(csvpath).T.to_dict()
 
