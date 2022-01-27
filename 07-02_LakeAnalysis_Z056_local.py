2e2e04a8cd8ba7e63420d0cd39ec8938f9f8290d
toddn <tcnichol@illinois.edu>
2022-01-27 15:11:58 -0600

    chaning location of forest and dem

--- 07-02_LakeAnalysis_Z056_local.py
+++ 07-02_LakeAnalysis_Z056_local.py
@@ -5,47 +5,51 @@ import os
 from matplotlib import pyplot as plt
 import geopandas as gpd
 
-PROCESSING_DIR = os.getcwd()
-tiles = ['150_62', '150_63']
-zone = 'Z056-Kolyma'
-directory_location = os.path.join(PROCESSING_DIR, 'process')
+PROCESS_ROOT = os.getcwd()
+
+tiles = ['150_62']
+process_dir = os.path.join(PROCESS_ROOT, 'process')
 site_name = 'Z056-Kolyma'
 
 CLASS_PERIOD = '2000-2020'
-CLASS_MODEL = os.path.join(PROCESSING_DIR, 'models', 'PDG_6idx2feat_elslope_model_py38_sklearn0232_v04.z')
-LAKE_FILTER_MODEL = os.path.join(PROCESSING_DIR, 'models', '20180820_lakefilter_12039samples_py3.z')
+CLASS_MODEL = os.path.join(PROCESS_ROOT, 'models', 'PDG_6idx2feat_elslope_model_py38_sklearn0232_v04.z')
+LAKE_FILTER_MODEL = os.path.join(PROCESS_ROOT, 'models', '20180820_lakefilter_12039samples_py3.z')
 
 os.environ['GDAL_BIN'] = ''
 os.environ['GDAL_PATH'] = ''
+#os.environ['GDAL_BIN'] = os.path.join(os.environ['CONDA_Prefix'], 'Library','bin')
+#os.environ['GDAL_PATH'] = os.path.join(os.environ['CONDA_Prefix'], 'Scripts') #in linux its the same as $GDAL_BIN
 
-DEM_LOCATION = os.path.join(PROCESSING_DIR, r'aux_data', 'DEM.vrt')
-FOREST_LOCATION = os.path.join(PROCESSING_DIR, r'aux_data', 'forestfire.vrt')
+DEM_LOCATION = os.path.join(PROCESS_ROOT, r'aux_data', 'dem', 'DEM.vrt')
+FOREST_LOCATION = os.path.join(PROCESS_ROOT, r'aux_data', 'forestfire', 'forestfire.vrt')
 
 def main():
 
-    tiles_directory = os.path.join(os.getcwd(), 'data', site_name, CLASS_PERIOD, 'tiles')
+    tiles_directory = os.path.join(PROCESS_ROOT, 'data', site_name, CLASS_PERIOD, 'tiles')
     tif_files = os.listdir(tiles_directory)
+
+    print('Available Images:\n')
     for t in tif_files:
         print(t)
 
-    l = LakeMaker(zone, os.path.join(directory_location, site_name), classperiod=CLASS_PERIOD)
+    l = LakeMaker(site_name, os.path.join(process_dir, site_name), classperiod='2000-2020')
     print("\nStart Classification")
     l.classify(CLASS_MODEL, tiles)
-    print("finished!")
-    """
-	print("\nPreparing additional Data")
-    l.prepare_aux_data(r'E:\18_Paper02_LakeAnalysis\02_AuxData\04_DEM\DEM.vrt',r'E:\18_Paper02_LakeAnalysis\02_AuxData\01_ForestLoss\forestfire.vrt')
-    """
 
+    print("\nPreparing additional Data")
+    # l.prepare_aux_data(r'E:\18_Paper02_LakeAnalysis\02_AuxData\04_DEM\DEM.vrt',r'E:\18_Paper02_LakeAnalysis\02_AuxData\01_ForestLoss\forestfire.vrt')
     l.prepare_aux_data(DEM_LOCATION, FOREST_LOCATION)
-
 
     print("\nCreating Masks")
     l.make_masks()
+
     print("\nCalculating Stats")
     l.make_stats()
+
     print("\nSaving DataFrame to Disk")
     l.save_df()
+
+    # errors come somewhere here
     print("\nFiltering non-lake objects")
     l.filter_data(LAKE_FILTER_MODEL)
     print("\nSaving DataFrame to Disk")
@@ -56,8 +60,6 @@ def main():
     l.save_results()
     print("\nSaving ResultGrid at 3km resolution")
     l.export_gridded_results([100, 250])
-    #print("\nSaving ResultGrid at 7.5km resolution")
-    #l.export_gridded_results(250)
 
 
 if __name__ == "__main__":
