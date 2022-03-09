775bbeec80fe31a178f3d4631c6303132d68be7b
toddn <tcnichol@illinois.edu>
2022-03-09 17:42:30 -0600

    add method to allow different class period and site names. to be called by extractor (currently empty)

--- 07-02_LakeAnalysis_Z056_local.py
+++ 07-02_LakeAnalysis_Z056_local.py
@@ -1,7 +1,22 @@
 from landsattrend.lake_analysis import LakeMaker
 import os, platform
+import shutil
 
 PROCESS_ROOT = os.getcwd()
+
+def add_file_to_tiles(path_to_file):
+    filename = path_to_file.split('/')[-1]
+    filename_parts = filename.split('_')
+    current_site_name = filename_parts[2]
+    current_class_period = filename_parts[1]
+    current_lat = filename_parts[3].lstrip('-')
+    current_lon = filename_parts[4].rstrip('.tif')
+
+    current_tiles_directory = os.path.join(PROCESS_ROOT, 'data', current_site_name, current_class_period, 'tiles')
+    new_filename = 'trendimage_'+current_site_name+'_'+current_lat+'_'+current_lon+'.tif'
+    destination = os.path.join(current_tiles_directory, new_filename)
+    shutil.copy(path_to_file, destination)
+
 
 def set_conda_gdal_paths():
     if platform.system() == 'Windows':
@@ -19,6 +34,43 @@ LAKE_FILTER_MODEL = os.path.join(PROCESS_ROOT, 'models', '20180820_lakefilter_12
 
 DEM_LOCATION = os.path.join(PROCESS_ROOT, r'aux_data', 'dem', 'DEM.vrt')
 FOREST_LOCATION = os.path.join(PROCESS_ROOT, r'aux_data', 'forestfire', 'forestfire.vrt')
+
+def run_lake_analuysis(path_to_tiles, current_class_period, current_site_name):
+    set_conda_gdal_paths()
+    tiles_directory = path_to_tiles
+    tif_files = os.listdir(tiles_directory)
+
+    print('Available Images:\n')
+    for t in tif_files:
+        print(t)
+
+    l = LakeMaker(site_name, os.path.join(process_dir, current_site_name), tiles_directory, classperiod=current_class_period)
+    print("\nStart Classification")
+    l.classify(CLASS_MODEL)
+
+    print("\nPreparing additional Data")
+    l.prepare_aux_data(DEM_LOCATION, FOREST_LOCATION)
+
+    print("\nCreating Masks")
+    l.make_masks()
+
+    print("\nCalculating Stats")
+    l.make_stats()
+
+    print("\nSaving DataFrame to Disk")
+    l.save_df()
+
+    # errors come somewhere here
+    print("\nFiltering non-lake objects")
+    l.filter_data(LAKE_FILTER_MODEL)
+    print("\nSaving DataFrame to Disk")
+    l.save_filtered_data()
+    print("\nTransforming data to metric values")
+    l.finalize_calculations()
+    print("\nSaving DataFrame to Disk")
+    l.save_results()
+    print("\nSaving ResultGrid at 3km resolution")
+    l.export_gridded_results([100, 250])
 
 def main():
     set_conda_gdal_paths()
