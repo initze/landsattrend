7ca82790070b5f69d04aa4995eb6adfff96ffa94
toddn <tcnichol@illinois.edu>
2023-03-21 12:25:52 -0500

    adding upload results wil

--- upload_landsat_results.py
+++ upload_landsat_results.py
@@ -9,11 +9,9 @@ import json
 landsat_space_id = '63051408e4b0fe3d54a9864e'
 alaska_collection_id = '63603f14e4b03d731ea3df5'
 
-path_to_data = '/scratch/bbki/toddn/landsat-delta/landsattrend/data'
+path_to_process = '/scratch/bbki/toddn/landsat-delta/landsattrend/process'
 
-path_to_process = '/scratch/bbki/toddn/landsat-delta/landsattrend/data'
-
-zones_to_upload = ['32602', '32603', '32604', '32605', '32606', '32607']
+zones_to_upload = ['32602', '32603', '32605', '32607']
 
 clowder_url = 'https://pdg.clowderframework.org'
 key = sys.argv[1]
@@ -69,14 +67,14 @@ def create_dataset(url, space, zone_name):
         return dataset_id
 
 def upload_a_file_to_dataset_with_folder(filepath, dataset_id, folder_name, url):
-    uploadedfileid = None
     folder = search_dataset_folders(dataset_id=dataset_id, folder_name=folder_name, url=url)
+    print("result of search dataset folders is", folder)
     if folder is None:
         folder_post_url = f"{url}/api/datasets/{dataset_id}/newFolder?key={key}"
         payload = json.dumps({'name': folder_name,
                               'parentId': dataset_id,
                               'parentType': "dataset"})
-        r = requests.post(folder_post_url, data=payload, headers=key)
+        r = requests.post(folder_post_url, data=payload, headers=headers)
         r.raise_for_status()
         folder_id = r.json()["id"]
     else:
@@ -88,6 +86,26 @@ def upload_a_file_to_dataset_with_folder(filepath, dataset_id, folder_name, url)
             m = MultipartEncoder(
                 fields={'file': (filename, open(filepath, 'rb')),
                         'folder_id':folder_id}
+            )
+            try:
+                result = requests.post(url, data=m, headers={'Content-Type': m.content_type},
+                                        verify=False)
+                print('upload result is', result)
+                uploadedfileid = result.json()['id']
+            except Exception as e:
+                print('failed to upload file, error')
+                print(e)
+    else:
+        print("unable to upload file %s (not found)", filepath)
+    return uploadedfileid
+
+def upload_a_file_to_dataset_with_folder_id(filepath, dataset_id, parentId, url):
+    url = '%s/api/uploadToDataset/%s?key=%s&folder_id=%s' % (url, dataset_id, key, parentId)
+    if os.path.exists(filepath):
+            filename = os.path.basename(filepath)
+            m = MultipartEncoder(
+                fields={'file': (filename, open(filepath, 'rb')),
+                        'folder_id':parentId}
             )
             try:
                 result = requests.post(url, data=m, headers={'Content-Type': m.content_type},
@@ -129,14 +147,39 @@ if __name__ == "__main__":
         zone_name = zones_to_upload[i]
         # create dataset for that zone in space
         zone_dataset = create_dataset(url=clowder_url, space=landsat_space_id, zone_name=zone_name)
-        path_to_zone_data = os.path.join(path_to_data, zone_name, '2000-2020', 'tiles')
-        zone_files = os.listdir(path_to_zone_data)
+        path_to_process = os.path.join(path_to_process, zone_name)
+        zone_folders = os.listdir(path_to_process)
         print('uploading these files for this zone')
-        print(zone_files)
-        for f in zone_files:
-            path_to_f = os.path.join(path_to_zone_data, f)
-            folder_name = 'process'
-            file_id = upload_a_file_to_dataset_with_folder(path_to_f, zone_dataset,'process', url=clowder_url)
-            file_id = upload_a_file_to_dataset(path_to_f, zone_dataset, url=clowder_url)
-            print('uploaded file', f, file_id)
+        process_folder = search_dataset_folders(dataset_id=zone_dataset, folder_name='process', url=clowder_url)
+        print('the process folder id is', process_folder, 'it already existed')
+        if process_folder is None:
+            process_folder_id = process_folder['id']
+            folder_post_url = f"{clowder_url}/api/datasets/{zone_dataset}/newFolder?key={key}"
+            print('folder post URL', folder_post_url)
+            payload = json.dumps({'name': 'process',
+                                  'parentId': zone_dataset,
+                                  'parentType': "dataset"})
+            r = requests.post(folder_post_url, data=payload, headers=headers)
+            r.raise_for_status()
+            process_folder_id = r.json()["id"]
+            print('process folder id is', process_folder_id,'we created it')
+        else:
+            process_folder_id = process_folder['id']
+        for folder in zone_folders:
+            path_to_folder = os.path.join(path_to_process, folder)
+            folder_post_url = f"{clowder_url}/api/datasets/{zone_dataset}/newFolder?key={key}"
+            print('folder post URL', folder_post_url)
+            payload = json.dumps({'name': folder,
+                                    'parentId': process_folder_id,
+                                    'parentType': "folder"})
+            r = requests.post(folder_post_url, data=payload, headers=headers)
+            r.raise_for_status()
+            folder_id = r.json()["id"]
+            files = os.listdir(path_to_folder)
+            for file in files:
+                path_to_file = os.path.join(path_to_folder, file)
+                print('uploading', path_to_file)
+                new_file_id = upload_a_file_to_dataset_with_folder_id(path_to_file, zone_dataset, folder_id, clowder_url)
+                print('the new file id is', new_file_id)
+            print('done')
 
