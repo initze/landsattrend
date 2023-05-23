9ea18fd1b1d7b4e7377eda3f3b9e3cd821c9ad6c
toddn <tcnichol@illinois.edu>
2023-05-23 14:02:49 -0500

    clowder utils will upload a site

--- import_export/clowder_utils.py
+++ import_export/clowder_utils.py
@@ -9,9 +9,15 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 url = sys.argv[1]
 key = sys.argv[2]
-landsat_space_id = '63051408e4b0fe3d54a9864e'
+landsat_space_id = '646d02d2e4b05d174c9fab1c'
 
-path_to_data = os.path.join(os.getcwd())
+current_dir = os.path.join(os.getcwd())
+data_dir = current_dir.replace('import_export', 'data')
+process_dir = current_dir.replace('import_export', 'process')
+current_zone = sys.argv[3]
+
+current_input = os.path.join(data_dir, current_zone)
+current_output = os.path.join(process_dir, current_zone)
 
 regions = {
     'TEST': {'Y_MIN_START': 62, 'Y_MIN_END': 64, 'X_MIN_START': 150, 'X_MIN_END': 156},
@@ -21,6 +27,125 @@ regions = {
     'EURASIA2': {'Y_MIN_START': 55, 'Y_MIN_END': 80, 'X_MIN_START': 66, 'X_MIN_END': 177},
     'EURASIA3': {'Y_MIN_START': 55, 'Y_MIN_END': 80, 'X_MIN_START': -180, 'X_MIN_END': -169},
 }
+
+client = pyclowder.datasets.ClowderClient(host=url, key=key)
+
+def upload_a_file_to_dataset(filepath, dataset_id, clowder_url, user_api):
+    url = '%s/api/uploadToDataset/%s?key=%s' % (clowder_url, dataset_id, user_api)
+    file_exists = os.path.exists(filepath)
+    before = datetime.now()
+    if os.path.exists(filepath):
+            filename = os.path.basename(filepath)
+            m = MultipartEncoder(
+                fields={'file': (filename, open(filepath, 'rb'))}
+            )
+            try:
+                result = requests.post(url, data=m, headers={'Content-Type': m.content_type},
+                                        verify=False)
+
+                uploadedfileid = result.json()['id']
+                return uploadedfileid
+            except Exception as e:
+                print('failed to upload file, error')
+                print(e)
+    else:
+        print(f"unable to upload file %s (not found) {filepath}")
+    return None
+
+
+def upload_a_file_to_dataset_with_folder(filepath, dataset_id, folder_id, clowder_url, user_api):
+    url = '%sapi/uploadToDataset/%s?key=%s&folder_id=%s' % (clowder_url, dataset_id, user_api, folder_id)
+    print('the url')
+    print(url)
+    file_exists = os.path.exists(filepath)
+    print('starting upload')
+    print(str(datetime.now()))
+    before = datetime.now()
+    if os.path.exists(filepath):
+            filename = os.path.basename(filepath)
+            m = MultipartEncoder(
+                fields={'file': (filename, open(filepath, 'rb')),
+                        'folder_id':folder_id}
+            )
+            try:
+                result = requests.post(url, data=m, headers={'Content-Type': m.content_type},
+                                        verify=False)
+
+                print(result)
+                uploadedfileid = result.json()['id']
+                print("uploaded file", uploadedfileid)
+                print(str(datetime.now()))
+                after = datetime.now()
+                duration = after - before
+                print(str(duration))
+            except Exception as e:
+                print('failed to upload file, error')
+                print(e)
+                print(str(datetime.now()))
+    else:
+        print("unable to upload file %s (not found)", filepath)
+
+
+def create_or_get_dataset_in_collection(dataset_name, collection_id, space_id):
+    print(f"get or create dataset {dataset_name} in collection {collection_id} in space{space_id}")
+    datasets_in_collection = client.get('/collections/'+collection_id+'/datasets')
+
+    ds = None
+
+    if len(datasets_in_collection) > 0:
+        for dataset in datasets_in_collection:
+            if dataset["name"] == dataset_name:
+                dataset_spaces = dataset["spaces"]
+                if space_id in dataset_spaces:
+                    return dataset
+                else:
+                    data = dict()
+                    data['space'] = space_id
+                    result = client.post('/spaces/'+space_id+'/addDatasetToSpace/'+dataset["id"], content=data)
+                    return dataset
+    if ds is None:
+        data = dict()
+        data["name"] = dataset_name
+        data["description"] = ''
+        if space_id:
+            data["space"] = [space_id]
+        if collection_id:
+            data["collection"] = [collection_id]
+        result = client.post("/datasets/createempty", content=data, params=data)
+        return result
+
+def create_or_get_dataset(dataset_name, space_id):
+    # get datasets in space
+    datasets_in_space = client.get('/spaces/' + space_id + '/datasets')
+    matching_dataset = None
+    for ds in datasets_in_space:
+        if ds['name'] == dataset_name:
+            matching_dataset = ds
+    if matching_dataset is None:
+        data = dict()
+        data["name"] = dataset_name
+        data["description"] = dataset_name
+        if space_id:
+            data["space"] = [space_id]
+        result = client.post("/datasets/createempty", content=data, params=data)
+        return result
+    else:
+        return matching_dataset
+
+def create_or_get_folder(dataset_id, folder_name):
+    # get datasets in space
+    ds_folders = client.get('/datasets/' + dataset_id + '/folders')
+    for folder in ds_folders:
+        if folder is not None:
+            current_folder_name = folder['name'].lstrip('/')
+            if current_folder_name == folder_name:
+                return folder
+    data = dict()
+    data["name"] = folder_name
+    data["parentType"] = "dataset"
+    data["parentId"] = dataset_id
+    new_folder = client.post("/datasets/" + dataset_id + '/newFolder', content=data, params=data)
+    return new_folder
 
 def get_region_for_filename(filename, regions=regions):
     filename_parts = filename.split('_')
@@ -36,4 +161,66 @@ def get_region_for_filename(filename, regions=regions):
             if Y_MIN_START <= Y_coord <= Y_MIN_END:
                 return region_name
     return None
+
+def get_region_for_filename(filename, regions=regions):
+    filename_parts = filename.split('_')
+    X_coord = int(filename_parts[3])
+    Y_coord = int(filename_parts[4].rstrip('.tif'))
+    region_names = regions.keys()
+    for region_name in region_names:
+        Y_MIN_START = regions[region_name]['Y_MIN_START']
+        Y_MIN_END = regions[region_name]['Y_MIN_END']
+        X_MIN_START = regions[region_name]['X_MIN_START']
+        X_MIN_END = regions[region_name]['X_MIN_END']
+        if X_MIN_START <= X_coord <= X_MIN_END:
+            if Y_MIN_START <= Y_coord <= Y_MIN_END:
+                return region_name
+    return None
+
+def process_input_dir(path_to_input):
+    parts = path_to_input.split('/')
+    site = parts[-1]
+    site_dataset = create_or_get_dataset(site, space_id=landsat_space_id)
+    print(site_dataset, 'is the current dataset')
+    site_dataset_id = site_dataset['id']
+    files = os.listdir(path_to_input)
+    for f in files:
+        path_to_file = os.path.join(path_to_input, f)
+        file_id = upload_a_file_to_dataset(filepath=path_to_file, dataset_id=site_dataset_id, clowder_url=url, user_api=key)
+        print('uploaded file', file_id)
+
+def process_input_dir(path_to_input):
+    parts = path_to_input.split('/')
+    site = parts[-1]
+    site_dataset = create_or_get_dataset(site, space_id=landsat_space_id)
+    print(site_dataset, 'is the current dataset')
+    site_dataset_id = site_dataset['id']
+    files = os.listdir(path_to_input)
+    for f in files:
+        path_to_file = os.path.join(path_to_input, f)
+        file_id = upload_a_file_to_dataset(filepath=path_to_file, dataset_id=site_dataset_id, clowder_url=url,
+                                           user_api=key)
+        print('uploaded file', file_id)
+
+def process_output_dir(path_to_output):
+    parts = path_to_output.split('/')
+    site = parts[-1]
+    site_dataset = create_or_get_dataset(site, space_id=landsat_space_id)
+    print(site_dataset, 'is the current dataset')
+    site_dataset_id = site_dataset['id']
+    folders = os.listdir(path_to_output)
+    for folder in folders:
+        path_to_folder = os.path.join(path_to_output, folder)
+        clowder_folder = create_or_get_folder(dataset_id=site_dataset_id, folder_name=folder)
+        clowder_folder_id = clowder_folder['id']
+        files = os.listdir(path_to_folder)
+        for f in files:
+            path_to_file = os.path.join(path_to_folder, f)
+            file_id = upload_a_file_to_dataset_with_folder(filepath=path_to_file, dataset_id=site_dataset_id, folder_id=clowder_folder_id, clowder_url=url, user_api=key)
+            print('uploaded file', file_id)
+
+if __name__ == '__main__':
+    process_input_dir(current_input)
+    process_output_dir(current_output)
+
 
