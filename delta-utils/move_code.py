2a63ac491e059cb32a5d23f0dbd782bd960a5b90
toddn <tcnichol@illinois.edu>
2023-05-26 14:44:42 -0500

    just print what we copy

--- delta-utils/move_code.py
+++ delta-utils/move_code.py
@@ -9,14 +9,12 @@ dir_contents = os.listdir(path_to_landsattrend)
 for content in dir_contents:
     content_path = os.path.join(path_to_landsattrend, content)
     if os.path.isfile(content_path):
-        print('file', content_path)
         new_path = os.path.join(path_to_projects, 'landsattrend', content)
         if content is 'setup.py':
             print('copying')
             print(content_path, new_path)
             shutil.copy(content_path, new_path)
     elif os.path.isdir(content_path):
-        print('dir', content_path)
         if content is not 'data' and content is not 'process':
             new_path = os.path.join(path_to_projects, 'landsattrend', content)
             if content is 'aux_data':
