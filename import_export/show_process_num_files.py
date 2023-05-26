f6e421b0762b929eb4364e4978649eb4b7eea72d
toddn <tcnichol@illinois.edu>
2023-05-26 13:49:05 -0500

    changing the file checks

--- import_export/show_process_num_files.py
+++ import_export/show_process_num_files.py
@@ -1,8 +1,33 @@
 import os
 import sys
-
+import pyclowder
+import pyclowder.datasets
 
 current_zone = sys.argv[1]
+url = sys.argv[2]
+key = sys.argv[3]
+client = pyclowder.datasets.ClowderClient(host= url, key=key)
+test_dataset_id = '646e8315e4b05d174ca074d6'
+
+landsat_space_id = '646d02d2e4b05d174c9fab1c'
+
+def get_matching_dataset_in_space(space_id, dataset_name):
+    datasets_in_space = client.get('/spaces/' + space_id + '/datasets')
+    matching_dataset = None
+    for ds in datasets_in_space:
+        if ds['name'] == dataset_name:
+            matching_dataset = ds
+            return matching_dataset
+    return matching_dataset
+
+files_in_dataset = client.get('/datasets/' + test_dataset_id + '/files')
+dataset_folders = client.get('/datasets/' + test_dataset_id + '/folders')
+folder_file_dict = dict()
+file_dict = dict()
+for file in files_in_dataset:
+    file_name = file['filename']
+    file_size = int(file['size'])
+    file_dict[file_name] = file_size
 
 current_dir = os.path.join(os.getcwd())
 data_dir = current_dir.replace('import_export', 'data')
@@ -13,7 +38,18 @@ current_output = os.path.join(process_dir, current_zone)
 print('getting the number of tiles')
 
 input_files = os.listdir(current_input)
-print(len(input_files), 'tiles')
+files_uploaded_correctly = []
+files_uploaded_wrong_size = []
+files_not_uploaded = []
+for input_file in input_files:
+    if input_file in file_dict:
+        input_file_path = os.path.join(current_input, input_file)
+        input_file_size = os.path.getsize(input_file_path)
+        if input_file_size == file_dict[input_file]:
+            files_uploaded_correctly.append(input_file)
+        else:
+            files_uploaded_wrong_size.append(input_file)
+
 
 print('checking each part of the process dir')
 process_folders = os.listdir(current_output)
@@ -22,4 +58,16 @@ for folder in process_folders:
     print('current folder is', folder)
     path_to_folder = os.path.join(current_output, folder)
     files_inside = os.listdir(path_to_folder)
-    print(folder, 'has', len(files_inside), 'files inside')
+    for output_file in files_inside:
+        if output_file in file_dict:
+            output_file_path = os.path.join(path_to_folder, output_file)
+            output_file_size = os.path.getsize(output_file_path)
+            if output_file_size == file_dict[output_file]:
+                files_uploaded_correctly.append(output_file)
+            else:
+                files_uploaded_wrong_size.append(output_file)
+
+print('printing summary')
+print('files uploaded that are the right size', len(files_uploaded_correctly))
+print('files uploaded, wrong size', len(files_uploaded_wrong_size))
+print('files not uploaded at all', len(files_not_uploaded))
