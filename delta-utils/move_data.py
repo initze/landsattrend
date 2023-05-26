b5e9b6cb85ed9a5bf1ddce9767ae15e676f52d27
toddn <tcnichol@illinois.edu>
2023-05-26 15:01:36 -0500

    right destination

--- delta-utils/move_data.py
+++ delta-utils/move_data.py
@@ -7,7 +7,6 @@ site_name = sys.argv[1]
 path_to_data = os.path.join('/scratch/bbou/toddn/landsat-delta/landsattrend/data/', site_name)
 print(path_to_data)
 
-destination = os.path.join('/projects/bbou/toddn/landsat/data', site_name)
-os.makedirs(os.path.dirname(destination), exist_ok=True)
+destination = os.path.join('/projects/bbou/toddn/landsatrrend/data', site_name)
 print(destination)
 shutil.copytree(path_to_data, destination)
