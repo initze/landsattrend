6f1a2053e9ad57075c3c9894aaa01239aa75d205
toddn <tcnichol@illinois.edu>
2023-05-26 15:06:01 -0500

    another typo

--- delta-utils/move_data.py
+++ delta-utils/move_data.py
@@ -7,6 +7,6 @@ site_name = sys.argv[1]
 path_to_data = os.path.join('/scratch/bbou/toddn/landsat-delta/landsattrend/data/', site_name)
 print(path_to_data)
 
-destination = os.path.join('/projects/bbou/toddn/landsatrend/data', site_name)
+destination = os.path.join('/projects/bbou/toddn/landsattrend/data', site_name)
 print(destination)
 shutil.copytree(path_to_data, destination)
