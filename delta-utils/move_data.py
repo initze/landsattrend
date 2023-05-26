33a38b96249563e04da8bb519ce3ab7f45a45a57
toddn <tcnichol@illinois.edu>
2023-05-26 14:51:30 -0500

    move data

---
+++ delta-utils/move_data.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import shutil
+
+site_name = sys.argv[1]
+
+path_to_data = os.path.join('/scratch/bbou/toddn/landsat-delta/landsattrend/data/', site_name)
+print(path_to_data)
+
+destination = os.path.join('/projects/bbou/toddn/landsat/data', site_name)
+print(destination)
+
+shutil.copytree(path_to_data, destination)
