47dc79705d80a5e01971c0526e2d91d6fb650f8a
toddn <tcnichol@illinois.edu>
2023-05-26 15:11:29 -0500

    fix command

--- delta-utils/delete_sbatch.py
+++ delta-utils/delete_sbatch.py
@@ -7,4 +7,4 @@ for content in contents:
     if content.endswith('.sbatch'):
         if content != 'start_gpu_job.sbatch':
             path_to_content = os.path.join(current_path, content)
-            os.path.remove(path_to_content)
+            os.remove(path_to_content)
