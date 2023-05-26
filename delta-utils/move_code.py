aa2915d5f9bfa485751f1dccc8d6ed2301993630
toddn <tcnichol@illinois.edu>
2023-05-26 14:35:29 -0500

    dir or file

--- delta-utils/move_code.py
+++ delta-utils/move_code.py
@@ -5,6 +5,10 @@ path_to_landsattrend = '/scratch/bbou/toddn/landsat-delta/landsattrend'
 print(path_to_landsattrend)
 path_to_projects = '/projects/bbou/toddn'
 
-all_files_in_dir = os.listdir(path_to_landsattrend)
-for file in all_files_in_dir:
-    print(file)
+dir_contents = os.listdir(path_to_landsattrend)
+for content in dir_contents:
+    content_path = os.path.join(path_to_landsattrend, content)
+    if os.path.isfile(content_path):
+        print('file', content_path)
+    elif os.path.isdir(content_path):
+        print('dir', content_path)
