92b3593444c74a08b47d28784912255825fba054
toddn <tcnichol@illinois.edu>
2023-06-09 14:55:36 -0500

    new way to activate conda

--- import_export/upload_region.sh
+++ import_export/upload_region.sh
@@ -2,13 +2,14 @@
 
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
+conda activate landsattrend2
+
 clowderUrl=$1
 key=$2
 currentRegion=$3
 pathToInputDelta=$4
 clowderSpaceId=$5
 
-
-conda activate landsattrend2
 
 python /scratch/bbou/toddn/landsat-delta/landsattrend/import_export/upload_input_regions.py  $clowderUrl $key $currentRegion $pathToInputDelta $clowderSpaceId
--- import_export/upload_region_output.sh
+++ import_export/upload_region_output.sh
@@ -6,6 +6,7 @@ currentRegion=$3
 pathToProcessDelta=$4
 clowderSpaceId=$5
 
+eval "$(conda shell.bash hook)"
 conda activate landsattrend2
 
 python /scratch/bbou/toddn/landsat-delta/landsattrend/import_export/upload_output_regions.py $clowderUrl $key $currentRegion $pathToProcessDelta $clowderSpaceId
