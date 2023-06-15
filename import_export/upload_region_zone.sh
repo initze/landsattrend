bd978d1db95eb338714cb8ebc6c3239d603733e5
toddn <tcnichol@illinois.edu>
2023-06-15 13:09:36 -0500

    output for zone

---
+++ import_export/upload_region_zone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clowderUrl=$1
+key=$2
+currentRegion=$3
+pathToProcessDelta=$4
+clowderSpaceId=$5
+
+eval "$(conda shell.bash hook)"
+conda activate landsattrend2
+
+python /scratch/bbou/toddn/landsat-delta/landsattrend/import_export/upload_output_regions.py $clowderUrl $key $zone $pathToProcessDelta $clowderSpaceId
