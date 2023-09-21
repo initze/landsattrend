a5cf2e07b4b3ee95189dc2e550fc87dd6732db6e
toddn <tcnichol@illinois.edu>
2023-09-21 14:33:30 -0500

    better check

--- delete_existing_sbatch.py
+++ delete_existing_sbatch.py
@@ -2,12 +2,13 @@ import os
 
 all_files = os.listdir(os.getcwd())
 
+existing_sbatch_files = ['start_gpu_job.sbatch', 'start_gpu_job.sbatch', 'start_gpu_job_test.sbatch']
+
 for file in all_files:
     print('checking file', file)
     if file.endswith('.sbatch'):
         print('this ends with sbatch', file)
-        if file != 'start_gpu_job.sbatch' and file != 'start_gpu_job_test.sbatch':
-            if file != 'start_gpu_test_file.sbatch':
-                current_path = os.path.join(os.getcwd(), file)
-                print('deleting file', current_path)
-                os.remove(current_path)
+        if file not in existing_sbatch_files:
+            current_path = os.path.join(os.getcwd(), file)
+            print('deleting file', current_path)
+            os.remove(current_path)
