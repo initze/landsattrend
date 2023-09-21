f04aced2c6202bbeaf6850d0fd9a00c35901b946
toddn <tcnichol@illinois.edu>
2023-09-21 14:31:20 -0500

    fix other typo

--- delete_existing_sbatch.py
+++ delete_existing_sbatch.py
@@ -6,7 +6,7 @@ for file in all_files:
     print('checking file', file)
     if file.endswith('.sbatch'):
         print('this ends with sbatch', file)
-        if file != 'start_gpu_job.sbatch' and file != 'start_gpu_job_test.batch':
+        if file != 'start_gpu_job.sbatch' and file != 'start_gpu_job_test.sbatch':
             if file != 'start_gpu_test_file.sbatch':
                 current_path = os.path.join(os.getcwd(), file)
                 print('deleting file', current_path)
