ff843be698e1b44cbf3597d52f2beba14036a58f
toddn <tcnichol@illinois.edu>
2023-09-21 14:30:21 -0500

    more print

--- delete_existing_sbatch.py
+++ delete_existing_sbatch.py
@@ -3,6 +3,7 @@ import os
 all_files = os.listdir(os.getcwd())
 
 for file in all_files:
+    print('checking file', file)
     if file.endswith('.batch'):
         if file != 'start_gpu_job.sbatch' and file != 'start_gpu_job_test.batch':
             if file != 'start_gpu_test_file.sbatch':
