55ddd432ba9be3bc530e648b67d55997ee41f92b
toddn <tcnichol@illinois.edu>
2024-09-21 14:59:18 -0500

    no time sleep

--- data-export-gee/download_zone_from_cloud.py
+++ data-export-gee/download_zone_from_cloud.py
@@ -98,7 +98,6 @@ if __name__ == "__main__":
         current_filename = current_full_filename_parts[-1]
         if SERACH_STRING in current_filename:
             print("we will download", current_filename)
-            time.sleep(30)
             download_location = get_download_location(current_filename, dir=DEFAULT_DOWNLOAD_DIR)
             blob = bucket.get_blob(blob.name)
             print('downloading to', download_location)
