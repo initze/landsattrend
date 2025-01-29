7b6193a24b3b9a64852f7a8e3433d6eac3828d46
toddn <tcnichol@illinois.edu>
2025-01-29 16:58:57 -0600

    fix path

--- export_tools/utils/utils_processing.py
+++ export_tools/utils/utils_processing.py
@@ -8,7 +8,7 @@ import os
 from google.cloud import storage
 current_dir = os.getcwd()
 service_account = "pdg-landsattrend@uiuc-ncsa-permafrost.iam.gserviceaccount.com"
-path_to_file = os.path.join(os.getcwd(),'export_tools', 'project-keys', 'uiuc-ncsa-permafrost-44d44c10c9c7.json')
+path_to_file = os.path.join(os.getcwd(), 'project-keys', 'uiuc-ncsa-permafrost-44d44c10c9c7.json')
 credentials = ee.ServiceAccountCredentials(service_account, path_to_file)
 storage_client = storage.Client.from_service_account_json(
     path_to_file)
