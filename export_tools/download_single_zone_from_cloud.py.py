9d08ccb6e4aaca382e1cd6b38f61c034e7b44900
toddn <tcnichol@illinois.edu>
2025-01-29 16:52:36 -0600

    fixing path

--- export_tools/download_single_zone_from_cloud.py.py
+++ export_tools/download_single_zone_from_cloud.py.py
@@ -13,7 +13,7 @@ current_dir = os.getcwd()
 print('current dir', current_dir)
 
 service_account = "pdg-landsattrend@uiuc-ncsa-permafrost.iam.gserviceaccount.com"
-path_to_file = os.path.join(os.getcwd(), 'export_tools', 'project-keys', 'uiuc-ncsa-permafrost-44d44c10c9c7.json')
+path_to_file = os.path.join(os.getcwd(), 'project-keys', 'uiuc-ncsa-permafrost-44d44c10c9c7.json')
 credentials = ee.ServiceAccountCredentials(service_account, path_to_file)
 storage_client = storage.Client.from_service_account_json(
     path_to_file)
