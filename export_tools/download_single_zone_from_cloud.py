2f56d608997d0f4148e39088195d1a26874abab3
toddn <tcnichol@illinois.edu>
2025-01-29 17:07:35 -0600

    download directories

--- export_tools/download_single_zone_from_cloud.py
+++ export_tools/download_single_zone_from_cloud.py
@@ -7,6 +7,8 @@ import numpy as np
 import sys
 import argparse
 from google.cloud import storage
+from pathlib import Path
+
 
 current_dir = os.getcwd()
 print('current dir', current_dir)
@@ -40,17 +42,28 @@ regions = {
     'EURASIA3': {'Y_MIN_START': 55, 'Y_MIN_END': 80, 'X_MIN_START': -180, 'X_MIN_END': -169},
 }
 
-DOWNLOAD_PATH = ''
+DOWNLOAD_PATH = '/work/hdd/bbou/toddn/landsat-delta/landsattrend/data/'
 
 def download_from_cloud(current_zone, current_start_year, current_end_year):
 
     bucket = storage_client.get_bucket(outputBucket)
     blob_list = bucket.list_blobs()
     substring = current_start_year + '-' + current_end_year + '_' + current_zone
+    year_span = current_start_year + '-' + current_end_year
     for blob in blob_list:
         current_name = blob.name
         if substring in current_name:
+            print(current_name)
             print("We should download this file.")
+            current_download_path_dir = os.path.join(DOWNLOAD_PATH, current_zone, year_span, 'tiles')
+            if os.path.exists(current_download_path_dir) and os.path.isdir(current_download_path_dir):
+                print("Directory already exists", current_download_path_dir)
+            else:
+                print('create dir', current_download_path_dir)
+                Path(current_download_path_dir).mkdir(parents=True, exist_ok=True)
+            path_for_file = os.path.join(current_download_path_dir, current_name)
+            print("Downloading to", path_for_file)
+            blob.download_to_filename(path_for_file)
 
 if __name__ == "__main__":
     zone = sys.argv[1]
